7211d3a240b0a052a4a5c10231a95a6ed4856d0a
shintyabachan <icinbachan15@gmail.com>
2019-05-27 22:31:22 +0700

    Updates

---
+++ precache-manifest.bcb2d614cc77a51dc90fa82b39dca5d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c673d306e3e17e9a60dcab00915413",
+    "url": "/icinTa/index.html"
+  },
+  {
+    "revision": "ee0797b3a6cde4723d05",
+    "url": "/icinTa/static/css/2.0f65aed4.chunk.css"
+  },
+  {
+    "revision": "1075e96ec60b770c48b6",
+    "url": "/icinTa/static/css/main.2fb3860e.chunk.css"
+  },
+  {
+    "revision": "ee0797b3a6cde4723d05",
+    "url": "/icinTa/static/js/2.33f641ea.chunk.js"
+  },
+  {
+    "revision": "1075e96ec60b770c48b6",
+    "url": "/icinTa/static/js/main.0fd8399f.chunk.js"
+  },
+  {
+    "revision": "04149d27b23d2b01ea26",
+    "url": "/icinTa/static/js/runtime~main.8cc85bfe.js"
+  }
+]);
